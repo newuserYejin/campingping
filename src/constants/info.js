7cd297b1390c9247ee6bcecf2ff8fce5e80dca0d
hellojoyworldz <hellojoyworldz@gmail.com>
2024-04-17 17:11:08 +0900

    github api 세팅 및 정보 불러오기, 맴버와 프로젝트정보 추가

---
+++ src/constants/info.js
@@ -0,0 +1,45 @@
+export const infoProject = [
+  {
+    title: "Project Name",
+    content: "캠핑어떄",
+  },
+  {
+    title: "Project period",
+    content: "2024.04.15 ~ 2024.04.21",
+  },
+  {
+    title: "Product owner",
+    content: "한도희",
+  },
+  {
+    title: "Scrum master",
+    content: "정아림",
+  },
+  {
+    title: "Members",
+    content: "곽선아, 정예진, 한상결",
+  },
+];
+
+export const infoMember = [
+  {
+    id: "doheeing",
+    name: "한도희",
+  },
+  {
+    id: "alim-ui-developer",
+    name: "정아림",
+  },
+  {
+    id: "hellojoyworldz",
+    name: "곽선아",
+  },
+  {
+    id: "newuserYejin",
+    name: "정예진",
+  },
+  {
+    id: "sanggyeol1",
+    name: "한상결",
+  },
+];
