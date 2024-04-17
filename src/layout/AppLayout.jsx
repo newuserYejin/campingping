3be202f8d80018cc6db280a124d6b042a2eb8384
hellojoyworldz <hellojoyworldz@gmail.com>
2024-04-17 17:21:23 +0900

    AppLayout Footer import 경로 변경

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
 
 import Header from './components/Header';
-import Footer from './components/Footer'
+import Footer from './components/Footer/Footer'
 
 const maxWidth = "";
 
