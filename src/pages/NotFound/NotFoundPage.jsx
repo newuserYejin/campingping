68fef6c79260de40469872bd644e3a82bd2a801c
doheeing <doheeehan@naver.com>
2024-04-19 12:41:20 +0900

    notfoundpage 완성

--- src/pages/NotFound/NotFoundPage.jsx
+++ src/pages/NotFound/NotFoundPage.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import "./NotFoundPage.style.css";
 
 const NotFoundPage = () => {
+   const navigate = useNavigate();
+   useEffect(() => {
+     const timer = setTimeout(() => navigate("/"), 10000);
+   }, []);
   return (
     <div className="not-found-page">
       <div className="error-message">
-        <h3>Oops!</h3>
-        <h1>404</h1>
-        <h4>이곳은 다음 영화를 위해 비어둔 공간입니다. </h4>
-        <h4>곧 새로운 영화와 함께 만나요 ! </h4>
+        <div className="error-quote">
+          목적지에 닿아야 행복해지는 것이 아니라 여행하는 과정에서 행복을 느낀다
+        </div>
+        <h3>아직 찾아내지 못한 페이지에요.</h3>
+
         <h4>10초 뒤에 홈페이지로 돌아갑니다.</h4>
         <Link to="/" className="error-page-button">
-          Back to Home
+          집으로 돌아기기
         </Link>
       </div>
     </div>
