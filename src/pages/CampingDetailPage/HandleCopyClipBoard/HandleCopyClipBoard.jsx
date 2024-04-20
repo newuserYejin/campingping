a2a491fadfb6719f8d6edc596f7a1ffabdfbc0f8
doheeing <doheeehan@naver.com>
2024-04-20 21:58:17 +0900

    detail page kakao talk 공유 기능, 클립보드 복사 기능 구현

---
+++ src/pages/CampingDetailPage/HandleCopyClipBoard/HandleCopyClipBoard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import "./HandleCopyClipBoard.style.css";
+
+const HandleCopyClipBoard = ({ data }) => {
+  const copyURLToClipboard = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("페이지의 주소가 복사되었습니다. 일행과 여정을 공유해보세요!");
+    } catch (e) {
+      alert("복사에 실패하였습니다");
+    }
+  };
+  return (
+    <div>
+      <button
+      className="clip-board-copy-button"
+        onClick={() =>
+          copyURLToClipboard(
+            `http://localhost:3002/campings/${data?.contentId}?keyword=${data?.facltNm}&lat=${data?.mapY}&lon=${data?.mapX}`
+          )
+        }
+      >
+        {" "}
+        <FontAwesomeIcon icon={faLink} />
+      </button>
+    </div>
+  );
+};
+
+export default HandleCopyClipBoard;
