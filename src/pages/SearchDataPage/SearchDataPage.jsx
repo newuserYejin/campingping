778d121aa77a96c5b7322c4e0820ec33502c69dc
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2024-04-20 15:05:33 +0900

    로딩아이콘 추가, 태그서치 훅 수정

--- src/pages/SearchDataPage/SearchDataPage.jsx
+++ src/pages/SearchDataPage/SearchDataPage.jsx
@@ -9,6 +9,7 @@ import ListCard from '../CampingListPage/components/ListCard/ListCard';
 import { Container } from 'react-bootstrap';
 import { Pagination } from "@mui/material";
 import TopBanner from '../Homepage/components/TopBanner';
+import { CircularProgress } from '@mui/material';
 
 const SearchDataPage = () => {
     const [page, setPage] = useState(1)
@@ -24,8 +25,12 @@ const SearchDataPage = () => {
     const { data, isLoading, isError, error } = useSearchDataQuery({ keyword, page })
     console.log('data?', data)
 
+
+
     if (isLoading) {
-        return <h1>Loading...</h1>;
+        return <div className="loading_search_wrap">
+            <CircularProgress />
+        </div>
     }
     if (isError) {
         return <h3>Error: {error.message}</h3>;
@@ -38,7 +43,7 @@ const SearchDataPage = () => {
             (!theme || item.themaEnvrnCl.includes(theme));
     }) || []
 
-    // let lengthOfFilteredData = filteredData?.length;
+    let lengthOfFilteredData = filteredData?.length;
 
     const facilityData = data?.items.item?.map((item, index) => (
         item.sbrsCl.split(',')
@@ -54,23 +59,23 @@ const SearchDataPage = () => {
             <Container maxWidth="lg">
 
                 <div>
-                
-                    <h2> {data?.totalCount}건의 검색 결과가 있습니다.</h2>
-                    <div>현재 페이지 : {page}</div>
+                    <div className='search-result-title'>
+                        <h2>{page}페이지: {lengthOfFilteredData}건의 검색 결과가 있습니다.</h2>
+                    </div>
                     <button onClick={() => { setPage(page - 1) }}>이전</button>
                     <button onClick={() => { setPage(page + 1) }}>다음</button>
-                    <Grid container spacing={2}>
-                        {
-                            filteredData.map((searchData, index) => (
+
+                    {
+                        filteredData.map((searchData, index) => (
 
 
-                                <Grid key={index}>
-                                    <ListCard data={searchData} facilityData={facilityData} index={index} />
-                                </Grid>
 
-                            ))
-                        }
-                    </Grid>
+                            <ListCard data={searchData} facilityData={facilityData} index={index} />
+
+
+                        ))
+                    }
+
 
                 </div>
             </Container>
