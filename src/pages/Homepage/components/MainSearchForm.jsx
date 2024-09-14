4e5aa4747039e346c09c14b22ec2af89033f2c4a
alim <dahye_a@naver.com>
2024-09-15 07:56:54 +0900

    메인 페이지 검색 [+]버튼 삭제

--- src/pages/Homepage/components/MainSearchForm.jsx
+++ src/pages/Homepage/components/MainSearchForm.jsx
@@ -2,15 +2,12 @@ import { useState, useEffect } from "react";
 import './MainSearchForm.style.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEarthAsia, faLocationDot, faTree, faMagnifyingGlass, faPlus, faPenNib, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { Container, Box, Input, Select, MenuItem, Stack, Button } from '@mui/material';
-import { Dialog, DialogTitle, DialogContent } from '@mui/material';
-import { FormGroup, FormControlLabel, Checkbox } from '@mui/material';
+import { Container, Box, Input, Select, MenuItem, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { regions } from '../../../constants/sigungu';
 import { search_detail_filters } from '../../../constants/info';
 
 const MainSearchForm = () => {
-  const [modalOpen, setModalOpen] = useState(false);
   const [keyword, setKeyword] = useState("")
   const navigate = useNavigate();
   const [selectedProvince, setSelectedProvince] = useState('');
@@ -38,18 +35,9 @@ const MainSearchForm = () => {
   const handleThemeChage = (e) => {
     setSelectedTheme(e.target.value)
   }
-  const handleClickOpen = () => setModalOpen(true);
-  const handleClickClose = () => setModalOpen(false);
-
-  const searchByDetailTag = (e) =>{
-    e.preventDefault()
-    navigate(`/search?q=${keyword}&province=${selectedProvince}&city=${selectedCity}&theme=${selectedTheme}&selectedDetailTag=${selectedDetailTag}`)
-    setSelectedDetailTag([])
-  }
 
   return (
-    <>
-      <Container maxWidth="lg" sx={{ 
+      <Container sx={{ 
           position:'relative', 
           paddingTop:{
             xs:'1.5rem',
@@ -113,69 +101,9 @@ const MainSearchForm = () => {
             <Button type="submit" variant="contained">
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </Button>
-            <Button type="button" variant="contained" onClick={() => handleClickOpen(true)}>
-              <FontAwesomeIcon icon={faPlus} />
-            </Button>
           </div>
         </Box>
       </Container>
-      <Dialog
-        maxWidth="xl"
-        open={ modalOpen }
-        className='mainDetailSearchModal'
-        onClose={() => handleClickClose()}
-      >
-        <DialogTitle
-          component="h4" 
-          sx={{
-            padding:0,
-            fontFamily:"'Spoqa Han Sans Neo', sans-serif",
-            fontSize: "1.25rem",
-            lineHeight:1.25,
-            marginBottom:"2rem",
-          }}
-        >
-          원하는 캠핑장을<br />상세하게 검색해보세요.
-          <span className='btn_modalClose' onClick={() => handleClickClose()}>
-            <FontAwesomeIcon icon={faXmark} />
-          </span>
-        </DialogTitle>
-        <Box component="form" onSubmit={searchByDetailTag}>
-        <DialogContent>
-          {search_detail_filters.map((filter) => 
-            <FormGroup key={filter.id}>
-              <h5>{filter.name}</h5>
-              <ul>
-                {filter.labels.map((label) => 
-                  <li key={label.id}>
-                    <FormControlLabel
-                      label={label.name}
-                      control={<Checkbox onClick={()=>{
-                        setSelectedDetailTag(prevTags => {
-                          const index = prevTags.indexOf(label.name);
-                          if (index > -1) {
-                              return prevTags.filter((_, i) => i !== index)
-                          } else {
-                              return [...prevTags, label.name]
-                          }
-                      });
-                      }}/>}
-                      name={filter.id}
-                      id={label.id}
-                    />
-                  </li>
-                )}
-              </ul>
-            </FormGroup>
-          )}
-          <Stack spacing={2} direction="row" className='formBtnDiv'>
-          <Button type="submit" className='btn_search'>검색</Button>
-            <Button type="button" className='btn_reset'>초기화</Button>
-          </Stack>
-        </DialogContent>
-        </Box>
-      </Dialog>
-    </>
   )
 }
 
