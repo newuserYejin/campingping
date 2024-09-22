951a4cb5dc8107ade118e0db19f1de73d1ef60d0
alim <dahye_a@naver.com>
2024-09-22 12:10:06 +0900

    필터 효과 수정

--- src/pages/Homepage/components/TagSearch.jsx
+++ src/pages/Homepage/components/TagSearch.jsx
@@ -99,10 +99,12 @@ const TagSearch = () => {
                     }}
                   />
                   <label htmlFor={tag.id}>
-                    <img 
-                      src={imageUrl} 
-                      alt={tag.text}  
-                    />
+                    <i>
+                      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm4.71,7.71-5,5a1,1,0,0,1-1.42,0l-2-2a1,1,0,0,1,1.42-1.42L11,12.59l4.29-4.3a1,1,0,0,1,1.42,1.42Z" fill="#23489d"/>
+                      </svg>
+                      <img src={imageUrl} alt={tag.text} />
+                    </i>
                     <span>{tag.text}</span>
                   </label>
                 </li>
