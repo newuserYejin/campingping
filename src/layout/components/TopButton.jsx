3094c28c90c18765e19bfb71572dbd0be37dc1a0
alim <dahye_a@naver.com>
2024-10-02 08:32:33 +0900

    기존 develop브랜치 내용 삭제

--- src/layout/components/TopButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TopButton = () => {
-  return (
-    <div>TopButton</div>
-  )
-}
-
-export default TopButton
--- src/redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import authenticateReducer from "./authenticateReduce";
-
-export default combineReducers({
-  auth: authenticateReducer,
-});
