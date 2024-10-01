cbcb8850bf80e7ce6b7a73c8f15d3ff8ab0e9a1f
jin <jin200126@gmail.com>
2024-10-02 00:30:20 +0900

    pw update

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
 import { authenticateAction } from "../../redux/actions/authencticateAction";
 import api from "../../utils/api.js";
 
@@ -16,9 +15,6 @@ import "./LoginPage.style.css";
 
 const LoginPage = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const authenticate = useSelector((state) => state.auth.authenticate);
 
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
@@ -65,7 +61,6 @@ const LoginPage = () => {
         // 성공 시 처리 로직 추가 (알림 띄우기)
         const { user, token } = response.data;
 
-        dispatch(authenticateAction.login(user)); // 사용자 정보 저장
         console.log("사용자 정보:", user);
         console.log("토큰:", token);
 
@@ -87,58 +82,54 @@ const LoginPage = () => {
         margin: "4em auto",
       }}
     >
-      {!authenticate ? (
-        <div className="login-form-wrap">
-          <MainTitle title="Login" />
-          <form onSubmit={handleSubmit}>
-            <TextField
-              inputRef={idInputRef}
-              onChange={(event) => setId(event.target.value)}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="id"
-              label="아이디(이메일)"
-              name="아이디"
-              error={loginIdError ? loginIdError : null}
-              helperText={loginIdError ? loginIdError : null}
+      <div className="login-form-wrap">
+        <MainTitle title="Login" />
+        <form onSubmit={handleSubmit}>
+          <TextField
+            inputRef={idInputRef}
+            onChange={(event) => setId(event.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="id"
+            label="아이디(이메일)"
+            name="아이디"
+            error={loginIdError ? loginIdError : null}
+            helperText={loginIdError ? loginIdError : null}
+          />
+          <TextField
+            inputRef={passwordInputRef}
+            onChange={(event) => setPassword(event.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="비밀번호"
+            label="비밀번호"
+            type="password"
+            id="비밀번호"
+            autoComplete="current-password"
+            error={loginPwError ? loginPwError : null}
+            helperText={loginPwError ? loginPwError : null}
+          />
+          <div className="BeforeLogin">
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="자동 로그인"
             />
-            <TextField
-              inputRef={passwordInputRef}
-              onChange={(event) => setPassword(event.target.value)}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="비밀번호"
-              label="비밀번호"
-              type="password"
-              id="비밀번호"
-              autoComplete="current-password"
-              error={loginPwError ? loginPwError : null}
-              helperText={loginPwError ? loginPwError : null}
-            />
-            <div className="BeforeLogin">
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="자동 로그인"
-              />
-              <Link to={"/signup"}>회원가입 -&gt;</Link>
-            </div>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              className="button-login"
-            >
-              로그인
-            </Button>
-          </form>
-        </div>
-      ) : (
-        "이미 로그인을 하셨습니다."
-      )}
+            <Link to={"/signup"}>회원가입 -&gt;</Link>
+          </div>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            className="button-login"
+          >
+            로그인
+          </Button>
+        </form>
+      </div>
     </Container>
   );
 };
