010a89e4439f91dce60849928f745d4a04840d43
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2024-10-02 10:49:55 +0900

    프록시 설정

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 const MONGO_URL = process.env.REACT_APP_MONGO_URL;
 const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
+const BACK_PROXY = process.env.BACKEND_PROXY;
 
 const api = axios.create({
     // baseURL: `${REACT_APP_PROD_BACKEND}/api`,
     // baseURL: LOCAL_BACKEND,
-    baseURL: MONGO_URL,
+    // baseURL: MONGO_URL,
+    baseURL: `${BACK_PROXY}/api`,
     headers: {
         'Content-Type': 'application/json',
         authorization: `Bearer ${localStorage.getItem('token')}`,
